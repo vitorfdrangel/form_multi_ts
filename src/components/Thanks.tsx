b939a07ecea7e7d5001493c7d7afda0d515119a6
Vitor <vitordrangel@outlook.com>
2024-09-20 20:55:25 -0300

    Criando componentes

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-type Props = {};
-
-const Thanks = (props: Props) => {
+const Thanks = () => {
   return <div>Thanks</div>;
 };
 
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const UserForm = () => {
   return (
     <div>
